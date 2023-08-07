8855d64f0d1144e7c012ce6dac9938c8f4505e91
antonellanieto <antonieca96@gmail.com>
2023-08-07 15:52:30 -0300

    done

--- notused/train_dos.py
+++ notused/train_dos.py
@@ -14,22 +14,19 @@ from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 
 df = pd.read_csv('df_steam.csv')
-
-
-
-from sklearn.ensemble import BaggingRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+print(df.dtypes)
 
 columns_for_X = ['early_access', 'metascore', 'genres_encoded', 'year']
+
 X = df[columns_for_X]
 y = df['price']
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize and fit the Bagging Regressor model with DecisionTreeRegressor as base estimator
-base_estimator = DecisionTreeRegressor(random_state=42)
-model = BaggingRegressor(base_estimator=base_estimator, random_state=42)
+
+# Initialize and fit the Random Forest Regressor model
+model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 # Make predictions
@@ -43,10 +40,6 @@ r2 = r2_score(y_test, y_pred)
 # Print the evaluation results
 print("Root Mean Squared Error (RMSE):", rmse)
 print("R-squared (R2) Score:", r2)
-
-
-
-
 
 
 
